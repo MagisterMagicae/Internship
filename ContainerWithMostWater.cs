db26bd4589f8e1cb69b1b0cf88dd5324322aac0a
Tristan <tj.mm.email@gmail.com>
2026-04-09 12:36:52 +0200

    ADD ContainerWithMostWater Task

---
+++ ContainerWithMostWater.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+public class Solution {
+    public int MaxArea(int[] height) {
+        
+        int foundAbsoluteMax = 0;
+        int earlyIndex = 0;
+        int lateIndex = height.Length-1;
+
+
+        while (earlyIndex < lateIndex)
+        { 
+            int shorterHeight =  Math.Min(height[earlyIndex], height[lateIndex]);
+            int maxWaterAtCurrentPosition = shorterHeight*(lateIndex-earlyIndex);
+            
+            foundAbsoluteMax = Math.Max(foundAbsoluteMax, maxWaterAtCurrentPosition);
+
+            if(height[earlyIndex] < height[lateIndex])
+            {
+                earlyIndex++;
+            } 
+            else 
+            {
+                lateIndex--;
+            }
+        }
+        
+        return foundAbsoluteMax;
+
+    }  
+}
